528c5afceaffe108230eb1863733ce0ab707a1f3
tabarino <tabarino@outlook.com>
2020-05-15 19:05:50 +0100

    Angular Built-In Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ import { Course } from './model/course';
 })
 export class AppComponent {
     courses = COURSES;
+    startDate = new Date(2020, 0, 12);
+    title = COURSES[1].description;
+    price = 9.99234523;
+    rate = 0.67;
+    coreCourse = COURSES[0];
 
     onCourseSelected(course: Course) {
         console.log(course);
