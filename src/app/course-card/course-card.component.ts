b274f993ecc1efe04e46ebf86a872189b2965006
tabarino <tabarino@outlook.com>
2020-05-17 00:20:16 +0100

    Angular View Encapsulation - Default vs Shadow DOM

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewEncapsulation } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
     selector: 'course-card',
     templateUrl: './course-card.component.html',
     styleUrls: ['./course-card.component.css']
+    // encapsulation: ViewEncapsulation.Emulated -> is the Default for Angular
+    // If you want to disable it, you can put as
+    // encapsulation: ViewEncapsulation.None
+    // It's possible also to use -> encapsulation: ViewEncapsulation.ShadowDom
+    // ShadowDom is similar to Emulated, but it has also its particularities
+    // The best option is always use ViewEncapsulation.Emulated
 })
 export class CourseCardComponent implements OnInit {
     @Input()
