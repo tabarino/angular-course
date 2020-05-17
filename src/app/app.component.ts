be0fca75847ec0b36bf97ac41d9f66d3859196bb
tabarino <tabarino@outlook.com>
2020-05-17 09:31:30 +0100

    The Angular HTTP Client - GET calls with Request Parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { COURSES } from '../db-data';
-import { Course } from './model/course';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -8,11 +7,21 @@ import { Course } from './model/course';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    courses = COURSES;
+    courses;
 
-    constructor() {
+    constructor(
+        private http: HttpClient
+    ) {
     }
 
     ngOnInit() {
+        const params = new HttpParams()
+            .set('page', '1')
+            .set('pageSize', '10');
+
+        this.http.get('api/courses', { params })
+            .subscribe(
+                courses => this.courses = courses['payload']
+            );
     }
 }
