6aa04b9b54af68708031443be990e7537af5a5d4
tabarino <tabarino@outlook.com>
2020-05-17 12:05:57 +0100

    Angular DI - Injection Tokens In Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, InjectionToken, OnInit } from '@angular/core';
 import { Course } from './model/course';
 import { CoursesService } from './services/courses.service';
 import { Observable } from 'rxjs';
+import { AppConfig, CONFIG_TOKEN } from './config';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +12,11 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
     courses$: Observable<Course[]>
 
-    constructor(private coursesService: CoursesService) {
+    constructor(
+        private coursesService: CoursesService,
+        @Inject(CONFIG_TOKEN) private config: AppConfig
+    ) {
+        console.log(config);
     }
 
     ngOnInit() {
