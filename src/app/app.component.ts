870346b05546bc261010648b9145ad6bf9e8a8a8
tabarino <tabarino@outlook.com>
2020-05-17 10:13:36 +0100

    Angular Custom Service - Fetching Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Course } from './model/course';
 import { CoursesService } from './services/courses.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -13,20 +11,10 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
     courses$: Observable<Course[]>
 
-    constructor(
-        private http: HttpClient,
-        private coursesService: CoursesService
-    ) {
+    constructor(private coursesService: CoursesService) {
     }
 
     ngOnInit() {
-        const params = new HttpParams()
-            .set('page', '1')
-            .set('pageSize', '10');
-
-        this.courses$ = this.http.get<Course[]>('api/courses', { params })
-            .pipe(
-                map(courses => courses['payload'])
-            );
+        this.courses$ = this.coursesService.loadCourses();
     }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CoursesService {
-    constructor() {
+    constructor(private http: HttpClient) {
+    }
+
+    loadCourses(): Observable<Course[]> {
+        const params = new HttpParams()
+            .set('page', '1')
+            .set('pageSize', '10');
+
+        return this.http.get<Course[]>('api/courses', { params })
+            .pipe(
+                map(courses => courses['payload'])
+            );
     }
 }
