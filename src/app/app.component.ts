0511e3f3f37a3cb309821d5d8195abdc87b6de4f
tabarino <tabarino@outlook.com>
2020-05-18 02:34:55 +0100

    Angular OnPush Change Detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,4 +31,21 @@ export class AppComponent implements OnInit {
                 () => alert('Course Saved')
             );
     }
+
+    onEditCourse() {
+        // When we turn on the ChangeDetectionStrategy.OnPush on the course-card component
+        // This line won't change the description
+        // Because the course object is the same
+        // this.courses[0].description = 'New Value';
+
+        // However, if we mutate the whole object, it will change anyway
+        // The course object uses @Input and On Push change detection check if this object has changed
+        // It's not recommended to change the object like that, because you are mutating the course object
+        const course = this.courses[0];
+        const newCourse = { ...course };
+
+        newCourse.description = 'New Value';
+
+        this.courses[0] = newCourse;
+    }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from '../model/course';
-import { CoursesService } from '../services/courses.service';
 
 @Component({
     selector: 'course-card',
     templateUrl: './course-card.component.html',
-    styleUrls: ['./course-card.component.css']
+    styleUrls: ['./course-card.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseCardComponent implements OnInit {
     @Input()
