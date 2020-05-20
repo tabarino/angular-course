cc8a117b0852cf593560887652bcf508b59b7cfc
tabarino <tabarino@outlook.com>
2020-05-20 22:56:25 +0100

    Angular Custom Pipes - Step-by-Step Implementation

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,17 +4,20 @@ import { CourseCardComponent } from './course-card/course-card.component';
 import { CourseImageComponent } from './course-image/course-image.component';
 import { HighlightedDirective } from './directives/highlighted.directive';
 import { NgxUnlessDirective } from './directives/ngx-unless.directive';
+import { FilterByCategoryPipe } from './shared/filter-by-category.pipe';
 
 @NgModule({
     declarations: [
         CourseCardComponent,
         CourseImageComponent,
         HighlightedDirective,
-        NgxUnlessDirective
+        NgxUnlessDirective,
+        FilterByCategoryPipe
     ],
     exports: [
         CourseCardComponent,
-        CourseImageComponent
+        CourseImageComponent,
+        FilterByCategoryPipe
     ],
     imports: [
         CommonModule
---
+++ src/app/courses/shared/filter-by-category.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Course } from '../model/course';
+
+@Pipe({
+    name: 'filterByCategory'
+})
+export class FilterByCategoryPipe implements PipeTransform {
+    transform(courses: Course[], category: string) {
+        return courses.filter(course => course.category === category);
+    }
+}
